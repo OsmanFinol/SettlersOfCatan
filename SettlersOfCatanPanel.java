2c7871e501ada9614d40f8c8f4ae28d52023bd89
OsmanFinol <93617089+OsmanFinol@users.noreply.github.com>
2022-04-04 07:18:50 -0500

    Update SettlersOfCatanPanel.java

--- SettlersOfCatanPanel.java
+++ SettlersOfCatanPanel.java
@@ -1,24 +1,17 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
-public class SettlersOfCatanPanel extends JPanel {
-	private BufferedImage buildingCost;
+public class SettlersOfCatanPanel extends JPanel implements MouseListener {
 	private GameState gs;
 
 	public SettlersOfCatanPanel() {
-		try {
-			buildingCost = ImageIO.read(SettlersOfCatanPanel.class.getResource("/Images/building_costs.png"));
-			
-		} catch (Exception e) {
-			System.out.println("Exception Error");
-			return;
-		}
-		
 		gs = new GameState();
 
 	}
@@ -27,8 +20,42 @@ public class SettlersOfCatanPanel extends JPanel {
 	public void paint(Graphics g) {
 		g.setColor(new Color(210, 180, 140, 250));
 		g.fillRect(0, 0, 2000, 2000);
-		//g.drawImage(buildingCost, 45, 50, 100, 150, null);
-		gs.paint(g);
+		gs.paintState(g);
+	}
+
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
