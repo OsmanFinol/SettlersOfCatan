11541bb427a48568084c68bfe667fc7e1dba88b9
OsmanFinol <93617089+OsmanFinol@users.noreply.github.com>
2022-04-04 07:18:14 -0500

    Update ResourceCard.java

--- ResourceCard.java
+++ ResourceCard.java
@@ -1,4 +1,28 @@
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class ResourceCard {
-
+	
+	String type;		//brick, grain, sheep, stone, wood
+	BufferedImage image;
+	
+	
+	public ResourceCard(String t) {
+		type = t;
+		try {
+			image = ImageIO.read(ResourceCard.class.getResource("/CardImages/" + t + ".jpg"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String getName() {
+		return type;
+	}
+	public BufferedImage getImage() {
+		return image;
+	}
+	
 }
