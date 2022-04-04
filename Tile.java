12997a1db6ac76e10a5f9e71d4337133ea1e2dc7
OsmanFinol <93617089+OsmanFinol@users.noreply.github.com>
2022-04-04 07:19:26 -0500

    Update Tile.java

--- Tile.java
+++ Tile.java
@@ -5,23 +5,26 @@ import javax.imageio.ImageIO;
 
 public class Tile {
 
-	private BufferedImage pic;
-	private int diceRoll;
-	private BufferedImage numPic;
-	private boolean hasRobber;
-	private String type;
-	private ResourceCard resource;
-	private int xCord;
-	private int yCord;
+	private BufferedImage pic;		//picture of tile
+	private int diceRoll;			//the dice roll needed to activate it
+	private BufferedImage numPic;	//number icon
+	private boolean hasRobber;		//to know whether robber is here or not, will be important in board class
+	private String type;			//desert, forest, mines, etc.....
+	private ResourceCard resource;		//the resource you get from it
+	private int xCord; 		//x coord on panel
+	private int yCord;		//y cord on panel
 	
 	
 	public Tile(String str) {
 		type = str;
+		hasRobber = false;
+		if (str.equals("Desert")) {
+			hasRobber = true;
+		}
 		try {
 			if (!(type.equals("na")))
 					pic = ImageIO.read(SettlersOfCatanPanel.class.getResource("/TileImages/" + str +".png"));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -42,6 +45,22 @@ public class Tile {
 	}
 	public int getYCord() {
 		return yCord;
+	}
+	
+	public void addRobber() {
+		hasRobber = true;
+	}
+	public void removeRobber() {
+		hasRobber = true;
+	}
+	public boolean isRobber() {
+		return hasRobber;
+	}
+	
+	
+	public void setNum(int dR) {
+		diceRoll = dR;
+		//set numPic to this later
 	}
 	
 	public String toString() {
