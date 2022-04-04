c9c16e22a8fa1fc28a94276c63caf25345c21b45
OsmanFinol <93617089+OsmanFinol@users.noreply.github.com>
2022-04-04 07:20:23 -0500

    Add files via upload

---
+++ Structure.java
@@ -0,0 +1,24 @@
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+public class Structure {
+
+	private int type;
+	private String col;
+	private BufferedImage struct;
+	private ArrayList<ResourceCard> yield;
+	private boolean nearHarbor;
+	
+	public Structure(int i, String c) {
+		type = i;
+		col = c;
+	}
+	
+	public void upgrade() {
+		if (type == 1) 
+			type = 2;
+	}
+	
+	
+	
+}
