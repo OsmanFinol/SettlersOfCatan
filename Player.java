79314c9b9f00554e7c9ef1b521cfb35880ce81a4
OsmanFinol <93617089+OsmanFinol@users.noreply.github.com>
2022-04-08 07:18:23 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Player {
 	
@@ -20,6 +21,7 @@ public class Player {
 	
 	public void buildStructure(int t, Intersection i) {
 		boolean canPay = false;
+		//t == 0 = road, t == 1 = settlement
 		if (t == 0) {
 			//checks if player has necessary resources
 			canPay = inventory.contains(new ResourceCard("Brick")) && inventory.contains(new ResourceCard("Wood"));
@@ -32,29 +34,46 @@ public class Player {
 		
 		if (canPay) {
 			Structure struct = new Structure(t, color); //makes the structure the player wants
-			//i.putStructure(struct);
+			i.putStructure(struct);
+			addVictoryPoints(t);
 			if (t==0) {
+				//removes the materials
 				inventory.remove(new ResourceCard("Brick"));
 				inventory.remove(new ResourceCard("Wood"));
 			}
 			else if (t==1) {
+				//removes the materials
 				inventory.remove(new ResourceCard("Brick"));
 				inventory.remove(new ResourceCard("Sheep"));
 				inventory.remove(new ResourceCard("Grain"));
 				inventory.remove(new ResourceCard("Wood"));
 			}
-			System.out.println("Built!");
 		}
 		else {
 			//implement something about not being able to pay
 			//:)
-			System.out.println("Can't Build");
 		}
 	}
 	
-	public void addResources(ArrayList<ResourceCard> arrList) {
+	public int numResources() {
+		return inventory.size();
+	}
+	
+	
+	public void addVictoryPoints(int i) { //adds victory points, self-explanatory
+		victoryPoints += i;
+	}
+	
+	public void addResources(ArrayList<ResourceCard> arrList) { //adds resource cards
 		inventory.addAll(arrList);
 	}
-
-    public ArrayList<Structure> getBuilds(){return builds;}
+	public void removeResources(ArrayList<ResourceCard> arrList) { //adds resource cards
+		inventory.removeAll(arrList);
+	}
+	
+	public String toString() {
+		return color;
+	}
+	
+	
 }
