db48bdf82128694c0c09ead3e013e2aeb2de103f
OsmanFinol <93617089+OsmanFinol@users.noreply.github.com>
2022-04-13 07:47:16 -0500

    Update Intersection.java

--- Intersection.java
+++ Intersection.java
@@ -5,7 +5,7 @@ public class Intersection {
 	private boolean hasStructure, hasHarbor;
 	private ArrayList<Tile> borders;
 	private Structure struct;
-
+	
 	public Intersection(int x, int y, boolean hasHarb, ArrayList<Tile> b) {
 		xCord = x;
 		yCord = y;
@@ -14,13 +14,21 @@ public class Intersection {
 		borders = b;
 		struct = null;
 	}
-
+	
 	public void putStructure(Structure s) {
 		struct = s;
 	}
-
-
-
-
-
+	
+	public int getXCord() {
+		return xCord;
+	}
+	public int getYCord() {
+		return yCord;
+	}
+	public ArrayList<Tile> getBorders() {
+		return borders;
+	}
+	
+	
+	
 }
