a475594450c7ac94f195e04cd3f50cf0cceee257
OsmanFinol <93617089+OsmanFinol@users.noreply.github.com>
2022-04-18 22:26:29 -0500

    Update PlayerManager.java

--- PlayerManager.java
+++ PlayerManager.java
@@ -9,12 +9,14 @@ public class PlayerManager {
 	ArrayList<Player> players;
 	int cPlayer;
 	TreeSet<Player> playerStanding;
+	int numP;
 	
 	public PlayerManager(int num) { //num of players playing
 		players = new ArrayList<>();
 		cPlayer = 0;
 		playerStanding = new TreeSet<Player>(new playerComparer());
 		Scanner scan = new Scanner("Red White Orange Blue"); //sets the player colors
+		numP = num;
 		for (int i = 0; i < num; i++) {
 			players.add(new Player(scan.next()));
 			playerStanding.add(players.get(i));
@@ -25,7 +27,7 @@ public class PlayerManager {
 	//switches to next player
 	public void nextPlayer() {
 		cPlayer++;
-		if (cPlayer == 4) {
+		if (cPlayer == numP) {
 			cPlayer = 0;
 		}
 	}
@@ -36,10 +38,13 @@ public class PlayerManager {
 	}
 	public Player getNextPlayer() {
 		int c = cPlayer+1;
-		if (c == 4) {
+		if (c == numP) {
 			c = 0;
 		}
 		return players.get(c);
+	}
+	public int getNumPlayers() {
+		return numP;
 	}
 	
 	public void trade(Player p, ArrayList<ResourceCard> getting, ArrayList<ResourceCard> giving) { 
@@ -74,7 +79,11 @@ public class PlayerManager {
 		return temp;
 	}
 	
-	
+	public void setColors(String[] colors) {
+		for (int i = 0; i < numP; i++) {
+			players.get(i).setColor(colors[i]);
+		}
+	}
 	
 	public void updateStanding() {
 		playerStanding.clear();
