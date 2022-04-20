ab29b2eadfc162fa3c7a644be1fb827168265f7a
SLHSCS214 <K1104294@students.katyisd.org>
2022-04-20 07:24:13 -0500

    Fixed syntax

--- PlayerManager.java
+++ PlayerManager.java
@@ -104,7 +104,7 @@ public class PlayerManager {
 		}
 		return null;
 	}
-	private ArrayList<ResourceCard> getPlayersHand(int index){return players.get(index).getInventory();}
+	public ArrayList<ResourceCard> getPlayersHand(int index){return players.get(index).getInventory();}
 
 	//ignore this, it's just so that the treeset works :)
 	public class playerComparer implements Comparator<Player>
