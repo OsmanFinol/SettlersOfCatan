f66a1090ab2cab033e8668e72b8cd5c2b9ee3699
OsmanFinol <93617089+OsmanFinol@users.noreply.github.com>
2022-04-25 07:22:35 -0500

    Update PlayerManager.java

--- PlayerManager.java
+++ PlayerManager.java
@@ -31,9 +31,7 @@ public class PlayerManager {
 			cPlayer = 0;
 		}
 	}
-	public int cPlayerIndex() {
-		return cPlayer;
-	}
+	
 	//returns player
 	public Player getCPlayer() {
 		return players.get(cPlayer);
