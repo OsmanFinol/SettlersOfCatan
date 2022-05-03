d9f0764724ba7e159687d382e5fa141614ad27c8
seepiii <93924225+seepiii@users.noreply.github.com>
2022-05-03 14:47:43 -0500

    Update Intersection.java

--- Intersection.java
+++ Intersection.java
@@ -1,11 +1,17 @@
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
 
 public class Intersection {
 	private int xCord, yCord;
 	private boolean hasStructure, hasHarbor;
 	private ArrayList<Tile> borders;
 	private Structure struct;
-	
+	private BufferedImage structImage;
+	private String color;
+	private String st;
 	public Intersection(int x, int y, boolean hasHarb, ArrayList<Tile> b) {
 		xCord = x;
 		yCord = y;
@@ -13,12 +19,52 @@ public class Intersection {
 		hasHarbor = hasHarb;
 		borders = b;
 		struct = null;
+		color = "";
+		st = "";
 	}
-	
+	public void setColor(String c) {
+		color = c;
+		
+	}
 	public void putStructure(Structure s) {
 		struct = s;
 	}
-	
+	public void setStructure(boolean b) {
+		hasStructure = b;
+	}
+	public void setSet(String s) {
+		st = s;
+	}
+	public String getSet() {
+		return st;
+	}
+	/*public void setImageStructure(BufferedImage b) {
+		structImage = b;
+	}*/
+	public void setImageStructure(int i) {
+		if (i == 1) {
+			try {
+				structImage = ImageIO.read(Intersection.class.getResource("/BuildImages/settlement_" + color.toLowerCase() + ".png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		if (i == 2) {
+			try {
+				structImage = ImageIO.read(Intersection.class.getResource("/BuildImages/city_" + color.toLowerCase() + ".png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	public BufferedImage getImageStructure() {
+		return structImage;
+	}
+	public boolean hasStructure() {
+		return hasStructure;
+	}
 	public int getXCord() {
 		return xCord;
 	}
@@ -29,8 +75,6 @@ public class Intersection {
 		return borders;
 	}
 	
-	public boolean hasHarbor() {
-		return hasHarbor;
-	}
+	
 	
 }
